0326e0187432035741c686634304369f39617562
JeanPaulSidi <paulsidi@outlook.com>
2024-01-30 17:05:00 -0800

    size

--- WindowsFormsApp17/Form1.Designer.cs
+++ WindowsFormsApp17/Form1.Designer.cs
@@ -90,7 +90,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RosyBrown;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 723);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.comboBox2);
